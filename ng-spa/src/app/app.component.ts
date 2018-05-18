2f765f17aff60ec12a75e418a2f90d4269c818da
Vmer <->
2018-05-18 03:02:50 -0700

    Structure app fusion of ng and .Net

--- ng-spa/src/app/app.component.ts
+++ ng-spa/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  constructor(private _httpService: Http) {}
+
+  apiValues: string[] = [];
+
+  ngOnInit() {
+    this._httpService.get('/api/core').subscribe(values => {
+      this.apiValues = values.json() as string[];
+    });
+  }
 }
